3773203e6727c90c3fd02df926e9dd6ea70867d0
LanaElf <117464113+LanaElf@users.noreply.github.com>
2022-12-26 17:29:07 +0700

    Add files via upload

---
+++ NovikovGallery/js/arrPhotos.js
@@ -0,0 +1,164 @@
+let arrPhotos = 
+[
+	{
+		id: 1,
+		name:"Raindrops",
+		eyeemId: 193376141,
+	},
+	{
+		id: 2,
+		name:"The Way Forward",
+		eyeemId:194015603,
+	},
+	{
+		id: 3,
+		name: "Sun Inside",
+		eyeemId:194026573,
+	},
+	{
+		id: 4,
+		name:"Oasis Of Warmth",
+		eyeemId:187999983,
+	},
+	{
+		id: 5,
+		name: "The Gaze",
+		eyeemId:187999989,
+	},
+	{
+		id: 6,
+		name: "What Is Freedom?",
+		eyeemId:167276347,
+	},
+	{
+		id: 7,
+		name:"Subtlety Of Nature",
+		eyeemId:165850112,
+	},
+	{
+		id: 8,
+		name:"The Space Of Tranquility",
+		eyeemId:166893829.,
+	},
+	{
+		id: 9,
+		name:"Backyard",
+		eyeemId:164452312,
+	},
+	{
+		id: 10,
+		name:"Green Lines",
+		eyeemId:161502852,
+	},
+	{
+		id: 11,
+		name:"Little Suns in her hands",
+		eyeemId:160342477,
+	},
+	{
+		id: 12,
+		name:"Died beauty",
+		eyeemId:158873311,
+	},
+	{
+		id: 13,
+		name:"Dry Rest",
+		eyeemId:158869811,
+	},
+	{
+		id: 14,
+		name: "Frail Past",
+		eyeemId:158868643,
+	},
+	{
+		id: 15,
+		name:"Frozen Homeland",
+		eyeemId:154039969,
+	},
+	{
+		id: 16,
+		name:"Light & Night",
+		eyeemId:152524501,
+	},
+	{
+		id: 17,
+		name:"Dirty perfectness",
+		eyeemId:147272397,
+	},
+	{
+		id: 18,
+		name:"Red",
+		eyeemId:146838129,
+	},
+	{
+		id: 19,
+		name:"The Curve",
+		eyeemId:146183829,
+	},
+	{
+		id: 20,
+		name:"Sky Cells",
+		eyeemId:143933719,
+	},
+	{
+		id: 21,
+		name:"Eye Contact",
+		eyeemId:143626864,
+	},
+	{
+		id: 22,
+		name:"In the supergreen",
+		eyeemId:143626846,
+	},
+	{
+		id:23,
+		name:"Through my eyes",
+		eyeemId:143374158,
+	},
+	{
+		id: 24,
+		name:"The orb of ligthness",
+		eyeemId:143374106,
+	},
+	{
+		id: 25,
+		name:"Gray Fog",
+		eyeemId:143374080,
+	},
+	{
+		id: 26,
+		name:"Sense of charm",
+		eyeemId:143373620,
+	},
+	{
+		id: 27,
+		name:"Chaos and symmetry",
+		eyeemId:143373508,
+	},
+	{
+		id: 28,
+		name:"City's lines",
+		eyeemId:143373421,
+	},
+	{
+		id: 29,
+		name:"Precise angle",
+		eyeemId:143373395,
+	},
+	{
+		id: 30,
+		name:"Exactitude",
+		eyeemId:143373393,
+	},
+	{
+		id: 31,
+		name:"Sacred geometry",
+		eyeemId:143373392,
+	},
+	{
+		id: 32,
+		name:"High angle view of autumn",
+		eyeemId:185397518,
+	},
+
+]
