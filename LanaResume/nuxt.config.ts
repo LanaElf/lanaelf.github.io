3fd5b5c20f2455fc618f616720d81d98829decd4
Lana Tamilova <tamilova.sa@lynxsoft.ru>
2025-06-16 19:40:42 +0700

    revert new build

--- LanaResume/nuxt.config.ts
+++ LanaResume/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/LanaResume/dist/'
+    baseURL: process.env.NODE_ENV === 'production' ? '/dist' : './'
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
